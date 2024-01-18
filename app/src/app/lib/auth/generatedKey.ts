8d4d2fa677cfb368e30c64255094b9fff885a25b
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-18 11:18:17 +0900

    feat: トークン発行の関数作成

---
+++ app/src/app/lib/auth/generatedKey.ts
@@ -0,0 +1,11 @@
+import { randomBytes } from 'crypto';
+
+export const generateSecureRandomString = (length: number = 32) => {
+    // バイト単位のランダムなデータを生成
+    const randomBytesData = randomBytes(Math.ceil(length / 2));
+
+    // バイトデータを16進数文字列に変換
+    const token = randomBytesData.toString('hex').slice(0, length);
+
+    return token;
+};
