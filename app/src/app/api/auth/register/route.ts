8f289eb99ac6f978bbc530cb948dbaa9bde10e29
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-18 11:18:02 +0900

    refactor: レスポンスでuserIdを返すように変更

--- app/src/app/api/auth/register/route.ts
+++ app/src/app/api/auth/register/route.ts
@@ -1,39 +1,57 @@
+import { generateSecureRandomString } from "@/app/lib/auth/generatedKey";
 import mysql_connection from "@/app/lib/db/connection";
 import { NextRequest } from "next/server";
 
 /**
  * ユーザー登録
- * @param request 
+ * @param request
  */
 export const POST = async (request: NextRequest) => {
-    const body = await request.json();
+  const body = await request.json();
 
-    if (
-        !body.user_name ||
-        !body.display_name ||
-        !body.email ||
-        !body.password
-    ) {
-      return new Response(JSON.stringify({ message: "空の入力値があります。" }), {
+  if (!body.user_name || !body.display_name || !body.email || !body.password) {
+    return new Response(
+      JSON.stringify({ message: "必要な情報が不足しています。" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  try {
+    const connection = await mysql_connection();
+    const query =
+      "INSERT INTO users (user_name, display_name, email, password) VALUES (?, ?, ?, SHA2(?, 256))";
+    const [result] = await connection.execute(query, [
+      body.user_name,
+      body.display_name,
+      body.email,
+      body.password,
+    ]);
+
+    // ユーザーが正常に登録された場合、result.insertIdを使用して新しいユーザーのIDを取得
+    const userId = (result as any).insertId as string;
+
+    return new Response(
+      JSON.stringify({
+        message: "ユーザーが正常に登録されました。",
+        userId: userId,
+        userName: body.user_name,
+        token: generateSecureRandomString()
+      }),
+      {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "サーバーエラーが発生しました。" }),
+      {
         status: 500,
         headers: { "Content-Type": "application/json" },
-      });
-    }
-  
-    try {
-      const connection = await mysql_connection();
-      const query =
-        "INSERT INTO users (user_name, display_name, email, password) VALUES (?, ?, ?, SHA2(?, 256))";
-      await connection.execute(query, [body.user_name, body.display_name, body.email, body.password]);
-  
-      return new Response(JSON.stringify({ message: "送信に成功しました。" }), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      return new Response(JSON.stringify({ message: "送信に失敗しました。" }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-}
+      }
+    );
+  }
+};
