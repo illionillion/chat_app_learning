1bfe23eaa003c3324bf56e00fa676c9e88a2daf9
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-24 14:54:55 +0900

    feat: いいねの処理実装

---
+++ app/src/app/api/posts/[post_id]/like/route.ts
@@ -0,0 +1,86 @@
+import { verifyAccessToken } from "@/app/lib/auth/saveToken";
+import mysql_connection from "@/app/lib/db/connection";
+import { RowDataPacket } from "mysql2";
+import { NextRequest } from "next/server";
+
+/**
+ * 投稿にいいねする
+ * @param request
+ * @param param1
+ * @returns
+ */
+export const POST = async (
+  request: NextRequest,
+  { params }: { params: { post_id: number } }
+) => {
+  const { post_id: postId } = params;
+  const { userId, token } = await request.json();
+
+  try {
+    // トークン検証
+    const isAuthenticated = await verifyAccessToken(userId, token);
+    if (!isAuthenticated) {
+      return new Response(
+        JSON.stringify({
+          status: 401,
+          message: "認証エラー。トークンが無効です。",
+        }),
+        { status: 401, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    // 投稿のいいね
+    const connection = await mysql_connection();
+
+    const [existingLikes] = (await connection.execute(
+      "SELECT * FROM likes WHERE user_id = ? AND post_id = ? AND is_unliked = 0",
+      [userId, postId]
+    )) as RowDataPacket[];
+
+    if (existingLikes.length > 0) {
+      // 既にいいねが存在する場合の処理
+      return new Response(
+        JSON.stringify({
+          message: "既に投稿にいいねされています。",
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    const query =
+      "INSERT INTO likes (user_id, post_id, created_at, is_unliked) VALUES (?, ?, now(), 0)";
+    const [result] = (await connection.execute(query, [
+      userId,
+      postId,
+    ])) as RowDataPacket[];
+
+    if (result.affectedRows === 0) {
+      return new Response(
+        JSON.stringify({
+          message: "投稿にいいねされませんでした。",
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    return new Response(undefined, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Like error:", error);
+    return new Response(
+      JSON.stringify({ message: "サーバーエラーが発生しました。" }),
+      {
+        status: 400, // 例: 400 Bad Request
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
