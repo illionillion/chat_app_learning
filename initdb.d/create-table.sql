d0537f0a093ae3c4f9023ec89bfa1bbc18ac47e4
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-24 14:54:44 +0900

    feat: いいね解除の実装のために`is_unliked`追加

--- initdb.d/create-table.sql
+++ initdb.d/create-table.sql
@@ -47,6 +47,7 @@ CREATE TABLE likes (
     user_id INT NOT NULL,
     post_id INT NOT NULL,
     created_at DATETIME NOT NULL,
+    is_unliked BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
