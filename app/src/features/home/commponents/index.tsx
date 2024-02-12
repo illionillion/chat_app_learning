1afc44ed8cfb39ae6e4550a8dfef23a9d4d6d6dc
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-12 16:56:19 +0900

    feat: ログイン状態時にトークンを確認する処理実装

--- app/src/features/home/commponents/index.tsx
+++ app/src/features/home/commponents/index.tsx
@@ -9,10 +9,38 @@ export const Home: FC = () => {
   const { userData, onLogout } = useContext(StateContext);
   const router = useRouter();
 
+  const checkToken = async () => {
+    try {
+      const response = await fetch('/api/auth/verify', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: userData?.userId,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userData?.token}`,
+        },
+      });
+
+      const json = await response.json();
+
+      // 正しくないならログアウト
+      if (!json.authenticated) {
+        onLogout();
+        router.push('/');
+        return;
+      }
+    } catch (error) {
+      console.error('verify', error);
+    }
+  };
+
   useEffect(() => {
     console.log(userData);
     if (userData && Object.values(userData).every((v) => !!v === true)) {
       console.log('ログイン済み');
+      // 認証
+      checkToken();
     } else {
       console.log('未ログイン');
       router.push('/login');
