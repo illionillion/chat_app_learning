2083596510aede8506bfa81f4f816a94bff6ff5f
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-12 16:55:33 +0900

    fix: JSON内のステータスコード削除

--- app/src/app/api/auth/verify/route.ts
+++ app/src/app/api/auth/verify/route.ts
@@ -9,7 +9,7 @@ export const POST = async (request: NextRequest) => {
 
   if (!userId || !token) {
     return new Response(
-      JSON.stringify({ status: 400, message: '必要な情報が不足しています。' }),
+      JSON.stringify({ message: '必要な情報が不足しています。' }),
       {
         status: 400,
         headers: { 'Content-Type': 'application/json' },
@@ -23,12 +23,12 @@ export const POST = async (request: NextRequest) => {
   const authenticated = await verifyAccessToken(userId, accessToken);
 
   if (authenticated) {
-    return new Response(JSON.stringify({ status: 200, authenticated: true }), {
+    return new Response(JSON.stringify({ authenticated: true }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
   } else {
-    return new Response(JSON.stringify({ status: 401, authenticated: false }), {
+    return new Response(JSON.stringify({ authenticated: false }), {
       status: 401,
       headers: { 'Content-Type': 'application/json' },
     });
