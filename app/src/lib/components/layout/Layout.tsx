38adc4a8696ce532357a5b3f27ffb85bb879fa42
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-18 19:46:59 +0900

    feat: ナビゲションバー完成

--- app/src/lib/components/layout/Layout.tsx
+++ app/src/lib/components/layout/Layout.tsx
@@ -1,33 +1,102 @@
 'use client';
-import { Box, HStack, Heading, Link } from '@yamada-ui/react';
-import type { FC, ReactNode } from 'react';
+import { Box, Button, HStack, List, ListItem } from '@yamada-ui/react';
+import { Bell, Home, LogOut, Mail, Search, UserRound } from 'lucide-react';
+import { useContext, type FC, type ReactNode } from 'react';
+import { StateContext } from '../state/authContext';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
+  const { onLogout } = useContext(StateContext);
+
   return (
     <>
-      <HStack
-        as='header'
-        position='sticky'
-        top={0}
-        w='100vw'
-        px={2}
-        py={3}
-        justify='space-between'
-      >
-        <Box>
-          <Heading size='md'>SNS</Heading>
+      <HStack w='100vw' h='100dvh'>
+        <List
+          w='xs'
+          h='full'
+          justifyContent='center'
+          px={2}
+          borderRight='1px solid black'
+        >
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              as='a'
+              variant='ghost'
+              href='/'
+              leftIcon={<Home />}
+            >
+              ホーム
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              as='a'
+              variant='ghost'
+              href='/'
+              leftIcon={<Search />}
+            >
+              検索
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              as='a'
+              variant='ghost'
+              href='/'
+              leftIcon={<Bell />}
+            >
+              通知
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              as='a'
+              variant='ghost'
+              href='/'
+              leftIcon={<Mail />}
+            >
+              DM
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              as='a'
+              variant='ghost'
+              href='/'
+              leftIcon={<UserRound />}
+            >
+              プロフィール
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              w='full'
+              justifyContent='left'
+              variant='ghost'
+              leftIcon={<LogOut />}
+              onClick={onLogout}
+            >
+              ログアウト
+            </Button>
+          </ListItem>
+        </List>
+        <Box w='full' h='full'>
+          {children}
         </Box>
-        <HStack as='nav' gap={4} alignItems='center'>
-          <Link href='/'>TOP</Link>
-          <Link href='/'>TOP</Link>
-          <Link href='/'>TOP</Link>
-        </HStack>
       </HStack>
-      <Box as='main'>{children}</Box>
     </>
   );
 };
