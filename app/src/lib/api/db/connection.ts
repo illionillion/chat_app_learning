c94ff31951c8e233883ad4f8b5dfe8494a1ac954
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-18 21:22:24 +0900

    fix: SQLに接続できなくなるエラー回避のため、同時接続制限数を設定

--- app/src/lib/api/db/connection.ts
+++ app/src/lib/api/db/connection.ts
@@ -1,17 +1,17 @@
-// src/lib/connection.ts
-
 import mysql from 'mysql2/promise';
 
-/**
- * db接続
- * @returns
- */
-const mysql_connection = async () =>
-  await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
+// 接続プールの作成
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 10, // 同時接続数の制限
+});
 
-export default mysql_connection;
+// 接続を取得する関数
+const getConnection = async () => {
+  return await pool.getConnection();
+};
+
+export default getConnection;
