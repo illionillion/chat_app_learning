3e968b1fdb1820b1359271cae9616a7f81b2d053
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-20 16:47:24 +0900

    fix: 空のフラグメント削除

--- app/src/lib/components/layout/Layout.tsx
+++ app/src/lib/components/layout/Layout.tsx
@@ -12,91 +12,89 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
   const { onLogout } = useContext(StateContext);
 
   return (
-    <>
-      <HStack w='100vw' h='100dvh' gap={0}>
-        <List
-          w='xs'
-          h='full'
-          justifyContent='center'
-          px={2}
-          borderRight='1px solid black'
-        >
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Home />}
-            >
-              ホーム
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Search />}
-            >
-              検索
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Bell />}
-            >
-              通知
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Mail />}
-            >
-              DM
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<UserRound />}
-            >
-              プロフィール
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              variant='ghost'
-              leftIcon={<LogOut />}
-              onClick={onLogout}
-            >
-              ログアウト
-            </Button>
-          </ListItem>
-        </List>
-        <Box w='full' h='full'>
-          {children}
-        </Box>
-      </HStack>
-    </>
+    <HStack w='100vw' h='100dvh' gap={0}>
+      <List
+        w='xs'
+        h='full'
+        justifyContent='center'
+        px={2}
+        borderRight='1px solid black'
+      >
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Home />}
+          >
+            ホーム
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Search />}
+          >
+            検索
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Bell />}
+          >
+            通知
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Mail />}
+          >
+            DM
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<UserRound />}
+          >
+            プロフィール
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            variant='ghost'
+            leftIcon={<LogOut />}
+            onClick={onLogout}
+          >
+            ログアウト
+          </Button>
+        </ListItem>
+      </List>
+      <Box w='full' h='full'>
+        {children}
+      </Box>
+    </HStack>
   );
 };
