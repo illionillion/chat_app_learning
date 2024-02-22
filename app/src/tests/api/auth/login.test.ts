0d4d9966d9d4804e45311639f312a97f53e8354c
nakata0047 <n.craft0047r@gmail.com>
2024-02-22 15:42:29 +0900

    fix:改行削除

--- app/src/tests/api/auth/login.test.ts
+++ app/src/tests/api/auth/login.test.ts
@@ -1,31 +1,27 @@
 import { testApiHandler } from 'next-test-api-route-handler';
-import * as appHandlerVerify from '@/app/api/auth/verify/route';
 import * as appHandlerLogin from '@/app/api/auth/login/route';
 
 describe('/api/auth/login', () => {
-
-    it('ログインできるか', async () => {
-        await testApiHandler({
-          appHandler: appHandlerLogin,
-          async test({ fetch }) {
-            const userData = {
-              userName: 'Yusuke',
-              password: 'password',
-            };
-            const response = await fetch({
-              method: 'POST',
-              body: JSON.stringify(userData),
-            });
-            const json = await response.json();
-            expect(response.status).toStrictEqual(200);
-            expect(json.userName).toStrictEqual(userData.userName);
-            expect(Object.keys(json).includes('userId')).toStrictEqual(true);
-            expect(
-              response.headers.get('Authorization')?.includes('Bearer '),
-            ).toStrictEqual(true);
-    
-            
-          },
+  it('ログインできるか', async () => {
+    await testApiHandler({
+      appHandler: appHandlerLogin,
+      async test({ fetch }) {
+        const userData = {
+          userName: 'Yusuke',
+          password: 'password',
+        };
+        const response = await fetch({
+          method: 'POST',
+          body: JSON.stringify(userData),
         });
-      });
-})
+        const json = await response.json();
+        expect(response.status).toStrictEqual(200);
+        expect(json.userName).toStrictEqual(userData.userName);
+        expect(Object.keys(json).includes('userId')).toStrictEqual(true);
+        expect(
+          response.headers.get('Authorization')?.includes('Bearer '),
+        ).toStrictEqual(true);
+      },
+    });
+  });
+});
