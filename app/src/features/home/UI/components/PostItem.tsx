b9d38626ae7da1b610ff9452e593d033ce7de95d
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-23 15:22:47 +0900

    feat: アイコン追加

--- app/src/features/home/UI/components/PostItem.tsx
+++ app/src/features/home/UI/components/PostItem.tsx
@@ -1,5 +1,17 @@
 import type { PostData } from '@/lib/types/PostData';
-import { Card, CardBody, CardHeader, ListItem, Text } from '@yamada-ui/react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  ListItem,
+  Text,
+  VStack,
+} from '@yamada-ui/react';
+import { Heart, MessageSquare, Repeat2 } from 'lucide-react';
 import type { FC } from 'react';
 
 export const PostItem: FC<PostData> = ({
@@ -7,6 +19,9 @@ export const PostItem: FC<PostData> = ({
   display_name,
   created_at,
   content,
+  like_count,
+  repost_count,
+  reply_count,
 }) => {
   // 日時文字列をDateオブジェクトに変換
   const date = new Date(created_at);
@@ -14,17 +29,50 @@ export const PostItem: FC<PostData> = ({
   convertJST.setHours(convertJST.getHours() - 9);
   const formattedTime = convertJST.toLocaleString('ja-JP').slice(0, -3);
   return (
-    <ListItem as={Card}>
-      <CardHeader>
-        <Text fontSize='xl'>{display_name}</Text>
-        <Text fontSize='md' color='blackAlpha.500'>
-          {user_name}
-        </Text>
-        <Text fontSize='sm' color='blackAlpha.500'>
-          {formattedTime}
-        </Text>
-      </CardHeader>
-      <CardBody>{content}</CardBody>
+    <ListItem as={Card} flexDir='row'>
+      <Box pt='md' pl='md'>
+        <Avatar />
+      </Box>
+      <VStack gap={0}>
+        <CardHeader pl='sm'>
+          <Text fontSize='xl'>{display_name}</Text>
+          <Text fontSize='md' color='blackAlpha.500'>
+            {user_name}
+          </Text>
+          <Text fontSize='sm' color='blackAlpha.500'>
+            {formattedTime}
+          </Text>
+        </CardHeader>
+        <CardBody pl='sm' pt={0}>
+          <Text as='pre'>{content}</Text>
+        </CardBody>
+        <CardFooter pl='sm'>
+          <Button
+            variant='ghost'
+            size='xs'
+            gap={1}
+            leftIcon={<MessageSquare size='16px' />}
+          >
+            {reply_count}
+          </Button>
+          <Button
+            variant='ghost'
+            size='xs'
+            gap={1}
+            leftIcon={<Repeat2 size='16px' />}
+          >
+            {repost_count}
+          </Button>
+          <Button
+            variant='ghost'
+            size='xs'
+            gap={1}
+            leftIcon={<Heart size='16px' />}
+          >
+            {like_count}
+          </Button>
+        </CardFooter>
+      </VStack>
     </ListItem>
   );
 };
