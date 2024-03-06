c83408c8bfbb3c5d1c66764b1499e36b22d79f56
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-07 00:36:31 +0900

    fix: ローディングアニメーション追加

--- app/src/lib/components/layout/Layout.tsx
+++ app/src/lib/components/layout/Layout.tsx
@@ -1,5 +1,13 @@
 'use client';
-import { Box, Button, HStack, List, ListItem } from '@yamada-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  HStack,
+  List,
+  ListItem,
+  Loading,
+} from '@yamada-ui/react';
 import { Bell, Home, LogOut, Mail, Search, UserRound } from 'lucide-react';
 import {
   useContext,
@@ -58,92 +66,94 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
     }
   }, [userData]);
 
-  return (
-    isChecked && (
-      <HStack w='100vw' h='100dvh' gap={0}>
-        <List
-          w='xs'
-          h='full'
-          justifyContent='center'
-          px={2}
-          borderRight='1px solid black'
-        >
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Home />}
-            >
-              ホーム
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Search />}
-            >
-              検索
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Bell />}
-            >
-              通知
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<Mail />}
-            >
-              DM
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              as='a'
-              variant='ghost'
-              href='/'
-              leftIcon={<UserRound />}
-            >
-              プロフィール
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              w='full'
-              justifyContent='left'
-              variant='ghost'
-              leftIcon={<LogOut />}
-              onClick={onLogout}
-            >
-              ログアウト
-            </Button>
-          </ListItem>
-        </List>
-        <Box w='full' h='full' overflowY='auto'>
-          {children}
-        </Box>
-      </HStack>
-    )
+  return isChecked ? (
+    <HStack w='100vw' h='100dvh' gap={0}>
+      <List
+        w='xs'
+        h='full'
+        justifyContent='center'
+        px={2}
+        borderRight='1px solid black'
+      >
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Home />}
+          >
+            ホーム
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Search />}
+          >
+            検索
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Bell />}
+          >
+            通知
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<Mail />}
+          >
+            DM
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            as='a'
+            variant='ghost'
+            href='/'
+            leftIcon={<UserRound />}
+          >
+            プロフィール
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button
+            w='full'
+            justifyContent='left'
+            variant='ghost'
+            leftIcon={<LogOut />}
+            onClick={onLogout}
+          >
+            ログアウト
+          </Button>
+        </ListItem>
+      </List>
+      <Box w='full' h='full' overflowY='auto'>
+        {children}
+      </Box>
+    </HStack>
+  ) : (
+    <Center w='100vw' h='100dvh'>
+      <Loading size='4xl' />
+    </Center>
   );
 };
